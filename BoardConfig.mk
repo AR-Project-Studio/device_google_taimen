ProtonAOSP/android_device_google_wahoo

Add recovery.wipe file.
Bug: 36427762
Test: lunch muskie-userdebug && make bootimage. Check
      $OUT/recovery/root/etc/recovery.wipe exists.
Change-Id: Id86c53d62d2b5a293f22b37fef3c64caa82f15f1

BoardConfig.mk
@@ -47,6 +47,9 @@ TARGET_RECOVERY_FSTAB := device/google/wahoo/fstab.hardware
 BOARD_USES_RECOVERY_AS_BOOT := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
+# Partitions (listed in the file) to be wiped under recovery.
+TARGET_RECOVERY_WIPE := device/google/wahoo/recovery.wipe
+
 ENABLE_CPUSETS := true
 
 TARGET_COPY_OUT_VENDOR := vendor

AR-Project-Studio/device_google_taimen

BoardConfig.mk
@@ -88,6 +88,9 @@ BOARD_LISA_TARGET_SCRIPTS := device/google/wahoo/lisa/
 # rounded_corner_radius in overlay/frameworks/base/packages/SystemUI/res/values/dimens.xml.
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 105
 
+# Partitions (listed in the file) to be wiped under recovery.
+TARGET_RECOVERY_WIPE := device/google/taimen/recovery.wipe
+
 TARGET_RECOVERY_UI_LIB := \
     librecovery_ui_taimen \
     libfstab