f26bf85984b8b543b84c9f1bbcc4e1f892a62cf2
Asriadi Rahim <asriadirahim03@gmail.com>
2024-06-08 21:29:33 +0800

    taimen: Initial Afterlife-OS makefiles and dependencies
    Signed-off-by: Asriadi Rahim <asriadirahim03@gmail.com>

--- afterlife_taimen.mk
+++ afterlife_taimen.mk
@@ -18,8 +18,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# Inherit some common AfterLife-OS stuff
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
+AFTERLIFE_GAPPS := true
+AFTERLIFE_MAINTAINER := SleepQueen
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_IS_PIXEL := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := true
+
+# EPPE
+TARGET_DISABLE_EPPE := true
+
+# Inherit device configuration
 $(call inherit-product, device/google/taimen/device.mk)
+$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/taimen/proprietary/device-vendor.mk)
 
 PRODUCT_PACKAGES += \
@@ -32,12 +46,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/google/taimen/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
-PRODUCT_RESTRICT_VENDOR_FILES := owner
-
 PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
 
 PRODUCT_MANUFACTURER := Google
-PRODUCT_BRAND := Android
-PRODUCT_NAME := aosp_taimen
+PRODUCT_BRAND := google
+PRODUCT_NAME := afterlife_taimen
 PRODUCT_DEVICE := taimen
-PRODUCT_MODEL := AOSP on taimen
+PRODUCT_MODEL := Pixel 2 XL
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=taimen \
+    PRIVATE_BUILD_DESC="taimen-user 11 RP1A.201005.004.A1 6934943 release-keys"
+
+BUILD_FINGERPRINT := google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
